5b8412e37245df079e2aabcee31e01e7bbed5530
Niki <n.s.manolitsi@gmail.com>
2020-12-17 21:09:53 +0000

    Refactor to common crud interface. Closes #11

--- pet-clinic-data/src/main/java/com/nikimanolitsi/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/nikimanolitsi/petclinic/services/PetService.java
@@ -2,14 +2,5 @@ package com.nikimanolitsi.petclinic.services;
 
 import com.nikimanolitsi.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
-
+public interface PetService extends CrudService<Pet, Long> {
 }
