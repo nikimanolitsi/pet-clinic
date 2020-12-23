5d80149b325925ea093d649d24a98dc2c509efd4
Niki <n.s.manolitsi@gmail.com>
2020-12-23 22:29:32 +0000

    Update BaseEntity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/com/nikimanolitsi/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/nikimanolitsi/petclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package com.nikimanolitsi.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     /**
      * Recommendation of Hibernate to use boxed types
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
