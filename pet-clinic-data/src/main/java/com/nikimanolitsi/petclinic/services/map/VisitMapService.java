590df77210efb05a0c0c6e8b200a1ab80c7f0077
Niki <n.s.manolitsi@gmail.com>
2020-12-24 13:02:15 +0000

    Implement Map Impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/nikimanolitsi/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,38 @@
+package com.nikimanolitsi.petclinic.services.map;
+
+import com.nikimanolitsi.petclinic.model.Visit;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findDyId(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+            || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/nikimanolitsi/petclinic/services/map/VisitService.java
@@ -0,0 +1,7 @@
+package com.nikimanolitsi.petclinic.services.map;
+
+import com.nikimanolitsi.petclinic.model.Visit;
+import com.nikimanolitsi.petclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
