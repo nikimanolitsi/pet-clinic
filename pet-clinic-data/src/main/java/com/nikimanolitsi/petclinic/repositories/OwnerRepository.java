c21a434cf4dea55b79afb5b26a7bbca7141a7feb
Niki <n.s.manolitsi@gmail.com>
2020-12-26 12:59:18 +0000

    Impl find owner. Closes #54

--- pet-clinic-data/src/main/java/com/nikimanolitsi/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/nikimanolitsi/petclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package com.nikimanolitsi.petclinic.repositories;
 import com.nikimanolitsi.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String name);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
