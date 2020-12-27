068d2baecb70a98763e16c8e9fc476079a40a68d
Niki <n.s.manolitsi@gmail.com>
2020-12-27 19:23:01 +0000

    Fix PetController, so as pet is added to the owner's pet list. Closes #87

--- pet-clinic-web/src/main/java/com/nikimanolitsi/petclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/nikimanolitsi/petclinic/controllers/PetController.java
@@ -68,12 +68,12 @@ public class PetController {
             result.rejectValue("name", "duplicate", "already exists");
         }
         owner.getPets().add(pet);
+        pet.setOwner(owner);
         if (result.hasErrors()) {
             model.put("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
             petService.save(pet);
-
             return "redirect:/owners/" + owner.getId();
         }
     }
