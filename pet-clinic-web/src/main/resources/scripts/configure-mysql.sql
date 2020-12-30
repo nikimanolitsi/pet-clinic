bd2109b72de967e86fa93380f969cb875a8d315a
Niki <n.s.manolitsi@gmail.com>
2020-12-30 22:25:04 +0000

    Add MySQL init script. Closes #77

---
+++ pet-clinic-web/src/main/resources/scripts/configure-mysql.sql
@@ -0,0 +1,31 @@
+## Use to run mysql db docker image
+# docker run --name nm-mysql -e MYSQL_ROOT_PASSWORD=password -d mysql
+
+# connect to mysql and run as root user
+#Create Databases
+CREATE DATABASE dev;
+CREATE DATABASE prod;
+
+#Create database service accounts
+CREATE USER 'dev_user'@'localhost' IDENTIFIED BY 'nikimanolitsi';
+CREATE USER 'prod_user'@'localhost' IDENTIFIED BY 'nikimanolitsi';
+CREATE USER 'dev_user'@'%' IDENTIFIED BY 'nikimanolitsi';
+CREATE USER 'prod_user'@'%' IDENTIFIED BY 'nikimanolitsi';
+
+#Database grants
+GRANT SELECT ON dev.* to 'dev_user'@'localhost';
+GRANT INSERT ON dev.* to 'dev_user'@'localhost';
+GRANT DELETE ON dev.* to 'dev_user'@'localhost';
+GRANT UPDATE ON dev.* to 'dev_user'@'localhost';
+GRANT SELECT ON prod.* to 'prod_user'@'localhost';
+GRANT INSERT ON prod.* to 'prod_user'@'localhost';
+GRANT DELETE ON prod.* to 'prod_user'@'localhost';
+GRANT UPDATE ON prod.* to 'prod_user'@'localhost';
+GRANT SELECT ON dev.* to 'dev_user'@'%';
+GRANT INSERT ON dev.* to 'dev_user'@'%';
+GRANT DELETE ON dev.* to 'dev_user'@'%';
+GRANT UPDATE ON dev.* to 'dev_user'@'%';
+GRANT SELECT ON prod.* to 'prod_user'@'%';
+GRANT INSERT ON prod.* to 'prod_user'@'%';
+GRANT DELETE ON prod.* to 'prod_user'@'%';
+GRANT UPDATE ON prod.* to 'prod_user'@'%';
